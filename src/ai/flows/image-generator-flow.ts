22b0b669fdef37cdafd7f680b9d6bab98f489fa8
ismail boulsane <iboulsane10@gmail.com>
2025-09-03 10:46:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/flows/image-generator-flow.ts
+++ src/ai/flows/image-generator-flow.ts
@@ -35,7 +35,7 @@ const generateImageFlow = ai.defineFlow(
         model: 'googleai/gemini-2.0-flash-preview-image-generation',
         prompt: input.prompt,
         config: {
-            responseModalities: ['IMAGE'],
+            responseModalities: ['TEXT', 'IMAGE'],
         }
     });
     
